b69d5456ba906a08e5c4ad203b773b63f1eb06a8
Xenex <timo.wolf@gmx.ch>
2026-06-05 16:34:48 +0200

    fix: reorder includes and align pointer style in heap.cpp
    The serial include path was shortened from `../drivers/serial.h` to `../serial.h` and includes reordered to group local headers first. Pointer declarations were reformatted to attach `*` to the variable name for consistency with project style conventions.

--- src/drivers/mem/heap.cpp
+++ src/drivers/mem/heap.cpp
@@ -1,7 +1,7 @@
 #include "heap.h"
-#include "pmm.h"
-#include "../drivers/serial.h"
+#include "../serial.h"
 #include "../utils/memory.h"
+#include "pmm.h"
 
 namespace heap {
 
@@ -11,16 +11,16 @@ static const size_t MIN_SPLIT = BLOCK_HEADER_SIZE + 16;
 struct block {
   size_t size;
   bool free;
-  block* prev;
-  block* next;
+  block *prev;
+  block *next;
 };
 
-static block* head = nullptr;
+static block *head = nullptr;
 
 void init(size_t size) {
   serial::printf("heap: requesting %u bytes from pmm\n", (uint32_t)size);
   size_t num_pages = (size + 4095) / 4096;
-  void* base = nullptr;
+  void *base = nullptr;
   while (num_pages > 0) {
     base = pmm::alloc_pages(num_pages);
     if (base)
@@ -36,7 +36,7 @@ void init(size_t size) {
   for (size_t i = 0; i < actual_size; i += 4)
     *(volatile uint32_t *)((uintptr_t)base + i) = 0;
 
-  head = (block*)base;
+  head = (block *)base;
   head->size = actual_size;
   head->free = true;
   head->prev = nullptr;
@@ -46,20 +46,20 @@ void init(size_t size) {
                  (uint64_t)head, (uint32_t)actual_size, (uint32_t)num_pages);
 }
 
-void* alloc(size_t size) {
+void *alloc(size_t size) {
   if (size == 0)
     return nullptr;
 
   size = (size + 15) & ~15;
 
-  block* cur = head;
+  block *cur = head;
   while (cur) {
     if (cur->free) {
       size_t avail = cur->size - BLOCK_HEADER_SIZE;
       if (avail >= size) {
         size_t leftover = avail - size;
         if (leftover >= MIN_SPLIT) {
-          block* nb = (block*)((uint8_t*)cur + BLOCK_HEADER_SIZE + size);
+          block *nb = (block *)((uint8_t *)cur + BLOCK_HEADER_SIZE + size);
           nb->size = leftover;
           nb->free = true;
           nb->prev = cur;
@@ -70,7 +70,7 @@ void* alloc(size_t size) {
           cur->size = BLOCK_HEADER_SIZE + size;
         }
         cur->free = false;
-        return (uint8_t*)cur + BLOCK_HEADER_SIZE;
+        return (uint8_t *)cur + BLOCK_HEADER_SIZE;
       }
     }
     cur = cur->next;
@@ -80,11 +80,11 @@ void* alloc(size_t size) {
   return nullptr;
 }
 
-void free(void* ptr) {
+void free(void *ptr) {
   if (!ptr)
     return;
 
-  block* b = (block*)((uint8_t*)ptr - BLOCK_HEADER_SIZE);
+  block *b = (block *)((uint8_t *)ptr - BLOCK_HEADER_SIZE);
   b->free = true;
 
   if (b->next && b->next->free) {
@@ -102,15 +102,15 @@ void free(void* ptr) {
   }
 }
 
-void* calloc(size_t num, size_t size) {
+void *calloc(size_t num, size_t size) {
   size_t total = num * size;
-  void* p = alloc(total);
+  void *p = alloc(total);
   if (p)
     utils::memory::memset(p, 0, total);
   return p;
 }
 
-void* realloc(void* ptr, size_t new_size) {
+void *realloc(void *ptr, size_t new_size) {
   if (!ptr)
     return alloc(new_size);
   if (new_size == 0) {
@@ -118,22 +118,22 @@ void* realloc(void* ptr, size_t new_size) {
     return nullptr;
   }
 
-  block* b = (block*)((uint8_t*)ptr - BLOCK_HEADER_SIZE);
+  block *b = (block *)((uint8_t *)ptr - BLOCK_HEADER_SIZE);
   size_t old_size = b->size - BLOCK_HEADER_SIZE;
 
   if (old_size >= new_size)
     return ptr;
 
-  void* new_ptr = alloc(new_size);
+  void *new_ptr = alloc(new_size);
   if (!new_ptr)
     return nullptr;
 
   utils::memory::memset(new_ptr, 0, new_size);
   for (size_t i = 0; i < old_size; i++)
-    ((uint8_t*)new_ptr)[i] = ((uint8_t*)ptr)[i];
+    ((uint8_t *)new_ptr)[i] = ((uint8_t *)ptr)[i];
 
   free(ptr);
   return new_ptr;
 }
 
-}
+} // namespace heap
